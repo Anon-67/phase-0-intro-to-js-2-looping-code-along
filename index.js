ba5e12f8f9f9b669bf5711e4305988e6298d9c78
Stefan Inman <stefan.inman@gmail.com>
2022-01-03 17:59:29 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,87 @@
 // Code your solutions in this file
+
+
+function writeCards(myOnlyFriends, eventsMyMomIgnoresMeFor) {
+    const bigUps = [];
+    for (let i = 0; i < myOnlyFriends.length; i++) {
+        bigUps.push(`Thank you, ${myOnlyFriends[i]}, for the wonderful ${eventsMyMomIgnoresMeFor} gift!`)
+    }
+
+    return bigUps;    
+}
+
+
+// console.log(writeCards(["Joey","Adam", "Simon"], "Christmas"));
+
+
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number);
+        number--;
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let age = 30; age < 40; age++){
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+// }
+
+
+
+
+
+// const gifts = ["teddy bear",'drone','doll'];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         debugger;
+//     }
+
+// // return(gifts);  -- why do we return gifts here?
+// }
+
+// wrapGifts(gifts);
